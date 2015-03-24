7d5fc02b40cd3eb9a2c47729144c1bbb907ca3d4
Bryn Waibel <bwaibel@brightcove.com>
2015-03-24 15:12:06 -0700

    Update 2015-03-24T22:11:57.317Z

--- gulp/tasks/live_deploy.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').watch;
-
-gulp.task('live_deploy', ['deploy'], function() {
-  console.log(config)
-  gulp.watch(config.src, ['deploy']);
-});
--- src/js/actions/DataActionCreators.js
+++
@@ -1,8 +0,0 @@
-var AppDispatcher = require('../dispatchers/AppDispatcher');
-var Constants = require('../constants/AppConstants');
-
-module.exports = {
-
-  
-
-};
